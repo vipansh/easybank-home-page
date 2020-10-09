f55e5abdcbf35229c18443509acd231f4710d997
Vipansh Thakur <vipansh@github.com>
2020-10-10 00:58:21 +0530

    Updates

---
+++ precache-manifest.304913045fdf45eb64d3514eb7c55d61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfd9bacc470a16e07706d0fcecc8cc8",
+    "url": "/easybank-home-page/index.html"
+  },
+  {
+    "revision": "55aa8a4e04e195077f6c",
+    "url": "/easybank-home-page/static/css/main.dbdc868b.chunk.css"
+  },
+  {
+    "revision": "d42d060882e7ae06aa27",
+    "url": "/easybank-home-page/static/js/2.82448a49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/easybank-home-page/static/js/2.82448a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55aa8a4e04e195077f6c",
+    "url": "/easybank-home-page/static/js/main.3db095d6.chunk.js"
+  },
+  {
+    "revision": "9b147cb7df4dc00132bc",
+    "url": "/easybank-home-page/static/js/runtime-main.28835a5e.js"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/easybank-home-page/static/media/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/easybank-home-page/static/media/image-mockups.edc63e85.png"
+  }
+]);
